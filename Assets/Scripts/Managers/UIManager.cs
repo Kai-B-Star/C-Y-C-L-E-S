6bf7347f2d564ca30bac48ce6a5b31db3374b701
Sky Mendes <sky.mendes.forest@gmail.com>
2025-05-28 13:06:00 +0100

    UI panel management

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,12 +1,22 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Application = UnityEngine.Application;
 
 public class UIManager : MonoBehaviour
 {
     #region Declarations
     public static UIManager instance;
+
+    [Header("UI Panels")]
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject options;
+    [SerializeField] private GameObject controls;
+    [SerializeField] private GameObject credits;
+    public enum MainMenuScreens { MainMenu, Options, Controls, Credits }
+    private Dictionary<MainMenuScreens, GameObject> mainMenuOrganize;
     #endregion
+
+    #region MonoBehaviour
     private void Awake()
     {
         if (instance == null)
@@ -18,6 +28,54 @@ public class UIManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    private void Start()
+    {
+        MainMenuDictionary();
+    }
+    #endregion
+
+    #region MainMenu
+    public void MainMenuDictionary()
+    {
+        mainMenuOrganize = new Dictionary<MainMenuScreens, GameObject>();
+        mainMenuOrganize.Add(MainMenuScreens.MainMenu, mainMenu);
+        mainMenuOrganize.Add(MainMenuScreens.Options, options);
+        mainMenuOrganize.Add(MainMenuScreens.Controls, controls);
+        mainMenuOrganize.Add(MainMenuScreens.Credits, credits);
+    }
+    public void ShowMainScreen(MainMenuScreens mainScreens)
+    {
+        foreach (var screen in mainMenuOrganize.Values)
+        {
+            if (screen != null) screen.SetActive(false);
+        }
+
+        if (mainMenuOrganize.ContainsKey(mainScreens) && mainMenuOrganize[mainScreens] != null)
+        {
+            mainMenuOrganize[mainScreens].SetActive(true);
+        }
+    }
+    public void MainMenu()
+    {
+        ShowMainScreen(MainMenuScreens.MainMenu);
+    }
+    public void Options()
+    {
+        ShowMainScreen(MainMenuScreens.Options);
+    }
+    public void Controls()
+    {
+        ShowMainScreen(MainMenuScreens.Controls);
+    }
+    public void Credits()
+    {
+        ShowMainScreen(MainMenuScreens.Credits);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    #endregion
 }
 
 //scene transition logic 
